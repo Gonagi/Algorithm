0768957a20ed46bf95d18f863a140ecca6fa8c57
Gonagi <kkh10002@naver.com>
2023-12-28 12:38:10 +0900

    12.28

--- 18_삼삼한_수_17252.cpp
+++ 18_삼삼한_수_17252.cpp
@@ -1,55 +1,19 @@
 // 삼삼한 수
 // 17252
 
-// #include <iostream>
-// #include <vector>
-// #define TRUE 1
-// #define FALSE 0
-
-// int main()
-// {
-//   std::vector<int> checkExp(20, FALSE);
-//   int num, powerNum = 1, check = 0;
-
-//   std::cin >> num;
-//   if (num == 0)
-//   {
-//     std::cout << "NO" << '\n';
-//     return 0;
-//   }
-
-//   while (num)
-//   {
-//     while (num > (powerNum * 3) && powerNum * 3 > 0)
-//     {
-//       powerNum *= 3;
-//       check++;
-//     }
-
-//     if (checkExp[check])
-//     {
-//       std::cout << "NO" << '\n';
-//       return 0;
-//     }
-//     checkExp[check] = TRUE;
-//     num -= powerNum;
-//     powerNum = 1;
-//     check = 0;
-//   }
-
-//   std::cout << "YES" << '\n';
-
-//   return 0;
-// }
-
+/*
 #include <iostream>
+#include <vector>
+#define TRUE 1
+#define FALSE 0
 
 int main()
 {
-  int num;
-  std::cin >> num;
+  std::vector<int> checkExp(20, FALSE);
+  int num, powerNum = 1, check = 0;
 
-  if (!num)
+  std::cin >> num;
+  if (num == 0)
   {
     std::cout << "NO" << '\n';
     return 0;
@@ -57,13 +21,51 @@ int main()
 
   while (num)
   {
-    if (num % 3 > 1)
+    while (num > (powerNum * 3) && powerNum * 3 > 0)
+    {
+      powerNum *= 3;
+      check++;
+    }
+
+    if (checkExp[check])
     {
       std::cout << "NO" << '\n';
       return 0;
     }
-    num /= 3;
+    checkExp[check] = TRUE;
+    num -= powerNum;
+    powerNum = 1;
+    check = 0;
   }
+
   std::cout << "YES" << '\n';
+
   return 0;
 }
+*/
+
+// #include <iostream>
+
+// int main()
+// {
+//   int num;
+//   std::cin >> num;
+
+//   if (!num)
+//   {
+//     std::cout << "NO" << '\n';
+//     return 0;
+//   }
+
+//   while (num)
+//   {
+//     if (num % 3 > 1)
+//     {
+//       std::cout << "NO" << '\n';
+//       return 0;
+//     }
+//     num /= 3;
+//   }
+//   std::cout << "YES" << '\n';
+//   return 0;
+// }
