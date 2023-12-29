9d3f5ab6f027d1e52bb881b7c7096ac3fdf54044
Gonagi <kkh10002@naver.com>
2023-12-29 20:26:04 +0900

    카드2(2164)

---
+++ 23_카드2_2164.cpp
@@ -0,0 +1,31 @@
+// 카드2
+// 2164
+
+/*
+#include <iostream>
+
+int main()
+{
+  int N, powNum = 1, exp = 0;
+
+  std::cin >> N;
+
+  if (N == 1)
+    std::cout << 1 << '\n';
+  else if (N == 2)
+    std::cout << 2 << '\n';
+  else
+  {
+    while (N > powNum)
+    {
+      powNum <<= 1;
+      exp++;
+    }
+
+    int lowerPowNum = 2 << (exp - 2);
+    int result = (N - lowerPowNum) * 2;
+    std::cout << result << '\n';
+  }
+  return 0;
+}
+*/
