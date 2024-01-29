f4184e209d32c2c05aa8912ca9eae9706e8be1c2
Gonagi <kkh10002@naver.com>
2024-01-30 00:59:42 +0900

    박스 나누기 게임(11867)

---
+++ 49_박스_나누기_게임_11867.cpp
@@ -0,0 +1,19 @@
+// 박스 나누기 게임
+// 11867
+
+/*
+#include <iostream>
+
+int main()
+{
+  int N, M;
+
+  std::cin >> N >> M;
+
+  if (N % 2 == 0 || M % 2 == 0)
+    std::cout << 'A' << '\n';
+  else
+    std::cout << 'B' << '\n';
+  return 0;
+}
+*/
