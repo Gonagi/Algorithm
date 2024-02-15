6a82922f6ba4b018a9b0f2e636113d0e67d9e3a4
Gonagi <kkh10002@naver.com>
2024-02-15 17:04:24 +0900

    교수가 된 현우(2468)

---
+++ 59_교수가_된_현우_3474.cpp
@@ -0,0 +1,33 @@
+// 교수가 된 현우
+// 3474
+
+/*
+#include <iostream>
+
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+
+  int T, N, cal;
+  long long sum = 0;
+
+  std::cin >> T;
+
+  while (T--)
+  {
+    std::cin >> N;
+    cal = 5;
+    sum = 0;
+
+    while (N / cal)
+    {
+      sum += N / cal;
+      cal *= 5;
+    }
+    std::cout << sum << '\n';
+  }
+  return 0;
+}
+*/
