79230e27c130db129760f1e6d411a6b5a5a3023c
김건학 <kkh10002@naver.com>
2025-01-16 20:33:09 +0900

    최적화

--- swea/보급로_1249.java
+++ swea/보급로_1249.java
@@ -1,73 +1,71 @@
 import java.io.*;
 import java.util.*;
- 
+
 public class Solution {
     static final BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
-    static final class Direction {
-        int y, x;
- 
-        Direction(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-    }
- 
-    static final Direction[] DIRECTIONS = { new Direction(1, 0), new Direction(-1, 0), new Direction(0, 1),
-        new Direction(0, -1) };
- 
+    static final int[][] DIRECTIONS = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     static int N;
     static int[][] map = new int[100][100];
- 
+
     public static void main(String[] args) throws Exception {
         int T = Integer.parseInt(BR.readLine().trim());
- 
+
         for (int testCase = 1; testCase <= T; testCase++) {
             N = Integer.parseInt(BR.readLine().trim());
             for (int y = 0; y < N; y++) {
-                int[] mapX = Arrays.stream(BR.readLine().trim().split("")).mapToInt(Integer::parseInt).toArray();
+                String mapX = BR.readLine().trim();
                 for (int x = 0; x < N; x++) {
-                    map[y][x] = mapX[x];
+                    map[y][x] = Character.getNumericValue(mapX.charAt(x));
                 }
             }
- 
+
             boolean[][] visited = new boolean[100][100];
             int[][] count = new int[100][100];
-            Queue<Direction> que = new LinkedList<>();
-            que.add(new Direction(0, 0));
- 
+            Queue<int[]> que = getInitialQueue(visited, count);
+
             while (!que.isEmpty()) {
-                Direction cur = que.poll();
-                visited[cur.y][cur.x] = true;
- 
-                for (Direction d : DIRECTIONS) {
-                    Direction next = new Direction(cur.y + d.y, cur.x + d.x);
-                    if (!canMove(next)) {
+                int[] cur = que.poll();
+                int cy = cur[0], cx = cur[1];
+                visited[cy][cx] = true;
+
+                for (int[] d : DIRECTIONS) {
+                    int ny = cy + d[0];
+                    int nx = cx + d[1];
+                    if (!canMove(ny, nx)) {
                         continue;
                     }
-                    if (cur.y == 0 && cur.x == 0) {
-                        visited[next.y][next.x] = true;
-                        count[next.y][next.x] = map[next.y][next.x];
-                        que.add(next);
-                        continue;
-                    }
- 
-                    if (!visited[next.y][next.x] && map[next.y][next.x] == 0) {
-                        visited[next.y][next.x] = true;
-                        count[next.y][next.x] = count[cur.y][cur.x];
-                        que.add(next);
-                    } else if (!visited[next.y][next.x]
-                        || count[next.y][next.x] > count[cur.y][cur.x] + map[next.y][next.x]) {
-                        visited[next.y][next.x] = true;
-                        count[next.y][next.x] = count[cur.y][cur.x] + map[next.y][next.x];
-                        que.add(next);
+                    if (!visited[ny][nx] && map[ny][nx] == 0) {
+                        visited[ny][nx] = true;
+                        count[ny][nx] = count[cy][cx];
+                        que.add(new int[]{ny, nx});
+                    } else if (!visited[ny][nx] || count[ny][nx] > count[cy][cx] + map[ny][nx]) {
+                        visited[ny][nx] = true;
+                        count[ny][nx] = count[cy][cx] + map[ny][nx];
+                        que.add(new int[]{ny, nx});
                     }
                 }
             }
             System.out.printf("#%d %d\n", testCase, count[N - 1][N - 1]);
         }
     }
- 
-    public static boolean canMove(final Direction next) {
-        return next.y >= 0 && next.y < N && next.x >= 0 && next.x < N;
+
+    private static Queue<int[]> getInitialQueue(final boolean[][] visited, final int[][] count) {
+        Queue<int[]> que = new ArrayDeque<>();
+
+        visited[0][0] = true;
+
+        que.add(new int[]{0, 1});
+        visited[0][1] = true;
+        count[0][1] = map[0][1];
+
+        que.add(new int[]{1, 0});
+        visited[1][0] = true;
+        count[1][0] = map[1][0];
+
+        return que;
+    }
+
+    public static boolean canMove(final int y, final int x) {
+        return y >= 0 && y < N && x >= 0 && x < N;
     }
 }
