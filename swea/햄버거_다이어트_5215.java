83c90bc91b6be752ce14f27895115eff7e0369ce
김건학 <kkh10002@naver.com>
2025-02-23 15:33:09 +0900

    부분집합 풀이 추가

--- swea/햄버거_다이어트_5215.java
+++ swea/햄버거_다이어트_5215.java
@@ -45,3 +45,52 @@ public class Solution {
     }
 }
 
+
+
+// 부분집합
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static int N, L, result;
+	static int[] scores, calories;
+
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int testCase = 1; testCase <= T; testCase++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			L = Integer.parseInt(st.nextToken());
+			scores = new int[N];
+			calories = new int[N];
+			result = 0;
+			for (int idx = 0; idx < N; idx++) {
+				st = new StringTokenizer(br.readLine());
+				scores[idx] = Integer.parseInt(st.nextToken());
+				calories[idx] = Integer.parseInt(st.nextToken());
+			}
+			solve(0, 0, 0);
+			bw.write("#" + testCase + " " + result + "\n");
+		}
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+	static void solve(int start, int totalScores, int totalCalories) {
+		if (start == N) {
+			result = Math.max(result, totalScores);
+			return;
+		}
+
+		solve(start + 1, totalScores, totalCalories);
+		if (totalCalories + calories[start] <= L) {
+			solve(start + 1, totalScores + scores[start], totalCalories + calories[start]);
+		}
+	}
+}
+
