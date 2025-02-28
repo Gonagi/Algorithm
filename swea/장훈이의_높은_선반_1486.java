5f569eac125ce0577de84888f67b57d9b450917a
김건학 <kkh10002@naver.com>
2025-02-28 21:00:21 +0900

    update

--- swea/장훈이의_높은_선반_1486.java
+++ swea/장훈이의_높은_선반_1486.java
@@ -37,3 +37,63 @@ public class Solution {
     return result;
     }
 }
+
+
+/*
+import java.io.*;
+import java.util.*;
+ 
+public class Solution {
+    static int N, B, result;
+    static int[] heights;
+ 
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        for (int testCase = 1; testCase <= T; testCase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+            result = Integer.MAX_VALUE;
+            heights = new int[N];
+ 
+            st = new StringTokenizer(br.readLine());
+            for (int idx = 0; idx < N; idx++) {
+                heights[idx] = Integer.parseInt(st.nextToken());
+            }
+ 
+            dfs(0, 0);
+            bw.write("#" + testCase + " " + (result - B) + "\n");
+        }
+ 
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+ 
+    static boolean dfs(int idx, int sum) {
+        if (sum == B) {
+            result = B;
+            return true;
+        }
+ 
+        if (sum > B) {
+            result = Math.min(result, sum);
+            return false;
+        }
+ 
+        if (idx == N) {
+            return false;
+        }
+        if (dfs(idx + 1, sum + heights[idx])) {
+            return false;
+        }
+        if (dfs(idx + 1, sum)) {
+            return false;
+        }
+ 
+        return false;
+    }
+}
+*/
