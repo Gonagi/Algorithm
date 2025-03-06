e596f76d46f30229eb1d88b96fb08eb06c3f90a2
김건학 <kkh10002@naver.com>
2025-03-06 19:58:02 +0900

    파핑파핑 지뢰찾기 1868

--- swea/파핑파핑_지뢰찾기_1868.java
+++ swea/파핑파핑_지뢰찾기_1868.java
@@ -1,124 +1,102 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
- 
+import java.io.*;
+import java.util.*;
+
 public class Solution {
-    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-    static final class Direction {
-    int y, x;
- 
-    public Direction(int y, int x) {
-        this.y = y;
-        this.x = x;
-    }
-    }
- 
-    static final Direction[] DIRECTIONS = { new Direction(0, 1), new Direction(1, 1), new Direction(1, 0),
-        new Direction(1, -1), new Direction(0, -1), new Direction(-1, -1), new Direction(-1, 0),
-        new Direction(-1, 1) };
- 
-    static int N;
- 
-    public static void main(String[] args) throws Exception {
-    int T = Integer.parseInt(br.readLine());
-    for (int testCase = 1; testCase <= T; testCase++) {
-        N = Integer.parseInt(br.readLine());
-        char[][] inputMap = new char[N][N];
-        int[][] map = new int[N][N];
-        input(inputMap);
-        calMap(inputMap, map);
-        int result = check(map);
- 
-        System.out.printf("#%d %d\n", testCase, result);
-    }
-    }
- 
-    private static int check(final int[][] map) {
-    int[][] visited = new int[N][N];
-    int count = calDependantArea(map, visited, 0);
-    count = calIndependantArea(map, visited, count);
-    return count;
-    }
- 
-    private static int calDependantArea(final int[][] map, int[][] visited, int count) {
-    for (int y = 0; y < N; y++) {
-        for (int x = 0; x < N; x++) {
-        if (map[y][x] == 0 && visited[y][x] == 0) {
-            bfs(map, visited, y, x);
-            count++;
-        }
-        }
-    }
-    return count;
-    }
- 
-    private static int calIndependantArea(final int[][] map, final int[][] visited, int count) {
-    for (int y = 0; y < N; y++) {
-        for (int x = 0; x < N; x++) {
-        if (visited[y][x] == -1 || map[y][x] == -1) {
-            continue;
-        }
-        count++;
-        }
-    }
-    return count;
-    }
- 
-    private static void bfs(final int[][] map, int[][] visited, int y, int x) {
-    Queue<Direction> que = new LinkedList<>();
-    que.add(new Direction(y, x));
- 
-    while (!que.isEmpty()) {
-        Direction cur = que.poll();
-        visited[cur.y][cur.x] = -1;
- 
-        for (Direction d : DIRECTIONS) {
-        Direction next = new Direction(cur.y + d.y, cur.x + d.x);
-        if (!canMove(next) || visited[next.y][next.x] == -1) {
-            continue;
-        }
-        if (map[next.y][next.x] == 0) {
-            visited[next.y][next.x] = -1;
-            que.add(next);
-            continue;
-        }
-        visited[next.y][next.x] = -1;
-        }
-    }
-    }
- 
-    private static void input(final char[][] inputMap) throws IOException {
-    for (int y = 0; y < N; y++) {
-        inputMap[y] = br.readLine().toCharArray();
-    }
-    }
- 
-    private static void calMap(final char[][] inputMap, final int[][] map) {
-    for (int y = 0; y < N; y++) {
-        for (int x = 0; x < N; x++) {
-        Direction cur = new Direction(y, x);
-        if (inputMap[cur.y][cur.x] == '*') {
-            map[cur.y][cur.x] = -1;
-            continue;
-        }
-        int count = 0;
-        for (Direction d : DIRECTIONS) {
-            Direction next = new Direction(y + d.y, x + d.x);
-            if (!canMove(next))
-            continue;
-            if (inputMap[next.y][next.x] == '*') {
-            count++;
-            }
-        }
-        map[cur.y][cur.x] = count;
-        }
-    }
-    }
- 
-    private static boolean canMove(final Direction dir) {
-    return dir.y >= 0 && dir.y < N && dir.x >= 0 && dir.x < N;
-    }
+	static int N;
+	static List<int[]> zeros;
+	static int[][] map,
+			directions = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } };
+	static boolean[][] visited;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int testCase = 1; testCase <= T; testCase++) {
+			N = Integer.parseInt(br.readLine());
+			zeros = new ArrayList<>();
+			map = new int[N][N];
+			visited = new boolean[N][N];
+
+			for (int y = 0; y < N; y++) {
+				String inputX = br.readLine();
+				for (int x = 0; x < N; x++) {
+					if (inputX.charAt(x) == '*') {
+						map[y][x] = -1;
+					}
+				}
+			}
+
+			for (int y = 0; y < N; y++) {
+				for (int x = 0; x < N; x++) {
+					if (map[y][x] == -1) {
+						continue;
+					}
+					int count = 0;
+					for (int[] d : directions) {
+						int nextY = y + d[0];
+						int nextX = x + d[1];
+						if (!canMove(nextY, nextX)) {
+							continue;
+						}
+						if (map[nextY][nextX] == -1) {
+							count++;
+						}
+					}
+					if (count == 0) {
+						zeros.add(new int[] { y, x });
+					}
+					map[y][x] = count;
+				}
+			}
+
+			int curCount = 0;
+			for (int[] zero : zeros) {
+				Queue<int[]> que = new ArrayDeque<>();
+				if (visited[zero[0]][zero[1]]) {
+					continue;
+				}
+				visited[zero[0]][zero[1]] = true;
+				que.add(zero);
+
+				while (!que.isEmpty()) {
+					int[] cur = que.poll();
+					int curY = cur[0];
+					int curX = cur[1];
+					
+					for (int[] d : directions) {
+						int nearY = curY + d[0];
+						int nearX = curX + d[1];
+						if (!canMove(nearY, nearX) || visited[nearY][nearX] || map[nearY][nearX] == -1) {
+							continue;
+						}
+						visited[nearY][nearX] = true;
+						if (map[nearY][nearX] == 0) {
+							que.add(new int[] { nearY, nearX });
+						}
+					}
+				}
+				curCount++;
+			}
+
+			for (int y = 0; y < N; y++) {
+				for (int x = 0; x < N; x++) {
+					if (!visited[y][x] && map[y][x] != -1) {
+						curCount++;
+					}
+				}
+			}
+
+			bw.write("#" + testCase + " " + curCount + "\n");
+		}
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+	static boolean canMove(int y, int x) {
+		return y >= 0 && y < N && x >= 0 && x < N;
+	}
 }
+
