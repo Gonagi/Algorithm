5abe7ce4427fafcf29b750e47fbd790644eac6b1
김건학 <kkh10002@naver.com>
2026-05-04 21:13:49 +0900

    수열 2497

---
+++ 정올/수열_2497.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] input = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int idx = 0; idx < N; idx++) {
+            input[idx] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        for (int idx = 0; idx < K; idx++) {
+            sum += input[idx];
+        }
+
+        int max = sum;
+
+        for (
+             int idx = K; idx < N; idx++) {
+            sum -= input[idx - K];
+            sum += input[idx];
+            max = Math.max(max, sum);
+        }
+
+        System.out.println(max);
+        br.close();
+    }
+}
+
