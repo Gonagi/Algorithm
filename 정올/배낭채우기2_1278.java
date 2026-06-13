5f837cda68a8f164149e1d4b113312c067e1e583
김건학 <kkh10002@naver.com>
2026-06-13 14:52:45 +0900

    배낭채우기2 1278

---
+++ 정올/배낭채우기2_1278.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static class Node {
+		int w, p;
+
+		public Node(int w, int p) {
+			this.w = w;
+			this.p = p;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+
+		Node[] nodes = new Node[N];
+		for (int idx = 0; idx < N; idx++) {
+			st = new StringTokenizer(br.readLine());
+			int w = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+			nodes[idx] = new Node(w, p);
+		}
+
+		int[] dp = new int[W + 1];
+
+		for (Node node : nodes) {
+			for (int weight = W; weight >= node.w; weight--) {
+				dp[weight] = Math.max(dp[weight], dp[weight - node.w] + node.p);
+			}
+		}
+
+		System.out.println(dp[W]);
+		br.close();
+	}
+}
+
